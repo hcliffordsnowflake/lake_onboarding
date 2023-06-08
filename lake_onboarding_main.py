5a1d0343b179b71dccbff879a1ec62f07b420df6
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-06-08 15:44:05 -0700

    Update lake_onboarding_main.py
    Connect to Snowflake

--- lake_onboarding_main.py
+++ lake_onboarding_main.py
@@ -1,7 +1,15 @@
 import streamlit
 import snowflake.connector
 from PIL import Image
-streamlit.title('CoDE On-Boarding Forms')
+streamlit.title('CoDE On-Boarding Form')
 
 image = Image.open('Data_Center_Logo.png')
 streamlit.image(image, caption='Center of Data Excellence')
+
+# Connect to Snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
