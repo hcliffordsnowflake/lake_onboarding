269425ba525493267c50e0e4d92c0c9b7706ed4a
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-06-08 15:39:34 -0700

    Update lake_onboarding_main.py

--- lake_onboarding_main.py
+++ lake_onboarding_main.py
@@ -1,7 +1,7 @@
 import streamlit
 import snowflake.connector
 from PIL import Image
-streamlit.title('CoDE On-Boarding Form')
+streamlit.title('CoDE On-Boarding Forms')
 
 image = Image.open('Data_Center_Logo.png')
 streamlit.image(image, caption='Center of Data Excellence')
