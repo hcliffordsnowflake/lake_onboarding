6a6e13ebc04837e22477149f0fc6a24c34e6446b
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-06-08 15:08:37 -0700

    Update lake_onboarding_main.py

--- lake_onboarding_main.py
+++ lake_onboarding_main.py
@@ -1,6 +1,6 @@
 import streamlit
 from PIL import Image
-streamlit.title('Center of Data Excellence On-Boarding Form')
+streamlit.title('CoDE On-Boarding Form')
 
 image = Image.open('Data_Center_Logo.png')
 streamlit.image(image, caption='Center of Data Excellence')
