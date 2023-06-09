2747115ecf5201912813a769acbad218532bb42e
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-06-09 12:55:14 -0700

    Update lake_onboarding_main.py
    Connect and Gather Business Units

--- lake_onboarding_main.py
+++ lake_onboarding_main.py
@@ -6,10 +6,10 @@ streamlit.title('CoDE On-Boarding Form Test')
 image = Image.open('Data_Center_Logo.png')
 streamlit.image(image, caption='Center of DATA Excellence')
 
-# Connect to Snowflake
+# Connect to Snowflake and retreive the business units
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
+my_cur.execute("SELECT BUSINESS_UNIT_DESCRIPTION FROM BUSINESS_UNIT")
+my_data_rows = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
